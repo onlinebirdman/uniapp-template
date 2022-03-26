3dcb7e5a10c813a4b410196f06284618deb3878d
liuzi <qiulz@wecut.com>
2022-03-26 17:39:44 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:vue/vue3-recommended', // vue3推荐规则
+    'standard', // 标准规则
+  ],
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
+    },
+  ],
+  rules: {
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always',
+      imports: 'never',
+      exports: 'never',
+      functions: 'never',
+    }],
+    semi: ['error', 'always'],
+  },
+};
---
+++ src/components/BannerSwiper/BannerSwiper.vue
@@ -0,0 +1,38 @@
+<template>
+  <swiper
+    class="swiper"
+    :indicator-dots="swiperConfig.indicatorDots"
+    :autoplay="swiperConfig.autoplay"
+    :interval="swiperConfig.interval"
+    :duration="swiperConfig.duration"
+  >
+    <swiper-item>
+      <view class="swiper-item uni-bg-red">
+        A
+      </view>
+    </swiper-item>
+    <swiper-item>
+      <view class="swiper-item uni-bg-green">
+        B
+      </view>
+    </swiper-item>
+    <swiper-item>
+      <view class="swiper-item uni-bg-blue">
+        C
+      </view>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script setup lang="ts">
+const swiperConfig = {
+  indicatorDots: true,
+  autoplay: true,
+  interval: true,
+  duration: 2000,
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
